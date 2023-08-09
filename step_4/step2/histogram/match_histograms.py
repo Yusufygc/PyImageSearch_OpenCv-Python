f51091d34e5da9a63e17dd03541cfaa5b2c11ae0
Yusuf Yağcı <98044490+Yusufygc@users.noreply.github.com>
2023-08-09 11:15:04 +0300

    histogram matching

--- step_4/step2/histogram/match_histograms.py
+++ step_4/step2/histogram/match_histograms.py
@@ -1,7 +1,8 @@
 """Unutmayın, burada yaptığımız renk dağılımını,referanstan alıp kaynağa aktarmaktır."""
 # import the necessary packages
 from skimage import exposure
-# Görüntü histogramlarını, kümülatif dağılım fonksiyonlarını hesaplamak ve histogram eşleştirmeyi uygulamak için scikit-image'ın pozlama kitaplığına ihtiyacımız var.
+# Görüntü histogramlarını, kümülatif dağılım fonksiyonlarını hesaplamak
+# ve histogram eşleştirmeyi uygulamak için scikit-image'ın pozlama kitaplığına ihtiyacımız var.
 import matplotlib.pyplot as plt
 import argparse
 import cv2
@@ -21,7 +22,9 @@ ref = cv2.imread(args["reference"])
 
 # determine if we are performing multichannel histogram matching
 # and then perform histogram matching itself
-"""Öncelikle, çok kanallı histogram eşleştirme işlemini gerçekleştirip gerçekleştirmediğimizi belirleyelim. Daha sonra histogram eşitleme işlemini uygulayalım.Çok kanallı histogram eşleştirme işlemini gerçekleştirip gerçekleştirmediğimizi belirleyelim."""
+# Çoklu kanal histogram eşleme işlemi yapıp yapmadığımızı 
+# belirleyelim ve ardından histogram eşleme işlemini gerçekleştirelim.
+
 
 print("[INFO] performing histogram matching...")
 multi = True if src.shape[-1] > 1 else False
@@ -37,6 +40,8 @@ cv2.waitKey(0)
 
 # construct a figure to display the histogram plots for each channel
 # before and after histogram matching was applied
+# histogram eşleştirme uygulanmadan önce ve sonra her kanal 
+# için histogram çizimlerini görüntülemek için bir şekil oluşturalım
 (fig, axs) =  plt.subplots(nrows=3, ncols=3, figsize=(8, 8))
 
 # loop over our source image, reference image, and output matched image
@@ -47,12 +52,14 @@ for (i, image) in enumerate((src, ref, matched)):
 	# loop over the names of the channels in RGB order
 	for (j, color) in enumerate(("red", "green", "blue")):
 		# compute a histogram for the current channel and plot it
+		# mevcut kanal için bir histogram hesaplayalım ve çizelim
 		(hist, bins) = exposure.histogram(image[..., j],
 			source_range="dtype")
 		axs[j, i].plot(bins, hist / hist.max())
 
 		# compute the cumulative distribution function for the
 		# current channel and plot it
+		# mevcut kanal için kümülatif dağılım fonksiyonunu hesaplayalım ve çizelim
 		(cdf, bins) = exposure.cumulative_distribution(image[..., j])
 		axs[j, i].plot(bins, cdf)
 
@@ -62,17 +69,17 @@ for (i, image) in enumerate((src, ref, matched)):
 		axs[j, 0].set_ylabel(color)
 
 """
-Line 34 creates a 3 x 3 figure to display the histograms of the Red, Green, and Blue channels for each of the src, ref, and matched images, respectively.
+Line 45 creates a 3 x 3 figure to display the histograms of the Red, Green, and Blue channels for each of the src, ref, and matched images, respectively.
 
-From there, Line 38 loops over each of our src, ref, and matched images. We then convert the current image from BGR to RGB channel ordering.
+From there, Line 48 loops over each of our src, ref, and matched images. We then convert the current image from BGR to RGB channel ordering.
 
 Next comes the actual plotting:
 
-Lines 45 and 46 compute a histogram for the current channel of the current image
-We then plot that histogram on Line 47
+Lines 56 and 57 compute a histogram for the current channel of the current image
+We then plot that histogram on Line 58
 
-Similarly, Lines 51 and 52 compute the cumulative distribution function of the current channel and then plot it
-Line 56 sets the y-axis label of the color
+Similarly, Lines 63 and 64 compute the cumulative distribution function of the current channel and then plot it
+Line 69 sets the y-axis label of the color
 The final step is to display the plot
 """
 
