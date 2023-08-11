ab17f2b96d5f3d6cf354bb558b5d2c6176d32b3e
Yusuf Yağcı <98044490+Yusufygc@users.noreply.github.com>
2023-08-11 15:30:15 +0300

    Color labeler

--- step_4/step10/determining_object_color/pyimage/colorlabeler.py
+++ step_4/step10/determining_object_color/pyimage/colorlabeler.py
@@ -14,7 +14,7 @@ class ColorLabeler:
 			"blue": (0, 0, 255)})
 		
 		# allocate memory for the L*a*b* image, then initialize
-		# the color names list
+		# the color names list (allocate = ayırmak, tahsis etmek)
 		self.lab = np.zeros((len(colors), 1, 3), dtype="uint8")
 		self.colorNames = []
 		
@@ -32,7 +32,7 @@ class ColorLabeler:
 
         Belirli bir renge sahip olduğunu düşündüğümüz görüntü bölgelerini etiketlemek ve işaretleme yapabilmek için, bilinen renklerimizin (yani lab dizisinin) veri kümemizle ve belirli bir görüntü bölgesinin ortalama renk değerleri arasındaki Öklidyen mesafesini hesaplayacağız.
 	
-        Öklidyen mesafeyi en küçükleyen bilinen renk, renk tanımlaması için seçilecektir.
+        Öklidyen mesafeyi en küçükleyen renk, renk tanımlaması için seçilecektir.
         """
 	def label(self, image, c):
 		# construct a mask for the contour, then compute the
@@ -43,12 +43,15 @@ class ColorLabeler:
 		mean = cv2.mean(image, mask=mask)[:3]
 		
 		# initialize the minimum distance found thus far
+		# şimdiye kadar bulunan minimum mesafeyi başlatalım.
 		minDist = (np.inf, None)
 		
 		# loop over the known L*a*b* color values
 		for (i, row) in enumerate(self.lab):
 			# compute the distance between the current L*a*b*
 			# color value and the mean of the image
+			# şu anki L*a*b* renk değeri ile
+			# görüntünün ortalaması arasındaki mesafeyi hesaplayalım.
 			d = dist.euclidean(row[0], mean)
 			
 			# if the distance is smaller than the current distance,
@@ -57,4 +60,5 @@ class ColorLabeler:
 				minDist = (d, i)
 				
 		# return the name of the color with the smallest distance
+		# en küçük mesafeye sahip rengin adını döndürür
 		return self.colorNames[minDist[1]]
