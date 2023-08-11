ea4e5daa046877e6ad5d5f8ee72e9487417ec478
Yusuf Yağcı <98044490+Yusufygc@users.noreply.github.com>
2023-08-11 21:40:45 +0300

    transform example

--- step_4/4point_getPerspective_transform/transform_example.py
+++ step_4/4point_getPerspective_transform/transform_example.py
@@ -12,15 +12,20 @@ ap.add_argument("-c", "--coords",
 args = vars(ap.parse_args())
 
 # load the image and grab the source coordinates (i.e. the list of
-# of (x, y) points)
+# (x, y) points)
 # NOTE: using the 'eval' function is bad form, but for this example
-# let's just roll with it -- in future posts I'll show you how to
-# automatically determine the coordinates without pre-supplying them
+# let's just roll with it.
+# Resmi yükleyelim ve kaynak koordinatlarını 
+# (örneğin, (x, y) noktaları listesi) alalım.
+#  NOTE: 'eval' fonksiyonunu kullanmak kötü bir yöntemdir, 
+# ancak bu örnekte şimdilik kabul edelim.
 image = cv2.imread(args["image"])
 pts = np.array(eval(args["coords"]), dtype = "float32")
 
 # apply the four point tranform to obtain a "birds eye view" of
 # the image
+# Dört nokta dönüşümünü uygulayarak 
+# görüntünün "kuş bakışı" görünümünü elde edelim.
 warped = four_point_transform(image, pts)
 
 # show the original and warped images
